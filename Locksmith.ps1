2b6a59ce57dc6d8d5c34b6fa020be0d74f5f5230
BP <gl4ssesbo1@protonmail.com>
2023-11-22 07:29:59 +0100

    Add files via upload

--- Locksmith.ps1
+++ Locksmith.ps1
@@ -186,7 +186,73 @@
 	$ADRoot = (Get-ADRootDSE -Server $server).defaultNamingContext
 	$ADCSObjects = (Get-ADObject -Server $server -Filter * -SearchBase "CN=Public Key Services,CN=Services,CN=Configuration,$ADRoot" -SearchScope 2 -Properties *)
 	
-	Set-AdditionalCAProperty -ADCSObjects $ADCSObjects
+	#Set-AdditionalCAProperty -ADCSObjects $ADCSObjects
+	
+	$ADCSObjects | Where-Object objectClass -Match 'pKIEnrollmentService' | ForEach-Object {
+		[string]$CAEnrollmentEndpoint = $_.'msPKI-Enrollment-Servers' | Select-String 'http.*' | ForEach-Object { $_.Matches[0].Value }
+		[string]$CAFullName = "$($_.dNSHostName)\$($_.Name)"
+		$CAHostname = $_.dNSHostName.split('.')[0]
+		# $CAName = $_.Name
+		if ($Credential) {
+			$CAHostDistinguishedName = (Get-ADObject -Server $server -Filter { (Name -eq $CAHostName) -and (objectclass -eq 'computer') } -Credential $Credential).DistinguishedName
+			$CAHostFQDN = (Get-ADObject -server $server -Filter { (Name -eq $CAHostName) -and (objectclass -eq 'computer') } -Properties DnsHostname -Credential $Credential).DnsHostname
+		} else {
+			$CAHostDistinguishedName = (Get-ADObject -server $server -Filter { (Name -eq $CAHostName) -and (objectclass -eq 'computer')}).DistinguishedName
+			$CAHostFQDN = (Get-ADObject -Filter -server $server { (Name -eq $CAHostName) -and (objectclass -eq 'computer') } -Properties DnsHostname).DnsHostname
+		}
+		$ping = Test-Connection -ComputerName $CAHostFQDN -Quiet -Count 1
+		if ($ping) {
+			try {
+				if ($Credential) {
+					$CertutilAudit = Invoke-Command -ComputerName $CAHostname -Credential $Credential -ScriptBlock { param($CAFullName); certutil -config $CAFullName -getreg CA\AuditFilter } -ArgumentList $CAFullName
+				} else {
+					$CertutilAudit = certutil -config $CAFullName -getreg CA\AuditFilter
+				}
+			} catch {
+				$AuditFilter = 'Failure'
+			}
+			try {
+				if ($Credential) {
+					$CertutilFlag = Invoke-Command -ComputerName $CAHostname -Credential $Credential -ScriptBlock { param($CAFullName); certutil -config $CAFullName -getreg policy\EditFlags } -ArgumentList $CAFullName
+				} else {
+					$CertutilFlag = certutil -config $CAFullName -getreg policy\EditFlags
+				}
+			} catch {
+				$AuditFilter = 'Failure'
+			}
+		} else {
+			$AuditFilter = 'CA Unavailable'
+			$SANFlag = 'CA Unavailable'
+		}
+		if ($CertutilAudit) {
+			try {
+				[string]$AuditFilter = $CertutilAudit | Select-String 'AuditFilter REG_DWORD = ' | Select-String '\('
+				$AuditFilter = $AuditFilter.split('(')[1].split(')')[0]
+			} catch {
+				try {
+					[string]$AuditFilter = $CertutilAudit | Select-String 'AuditFilter REG_DWORD = '
+					$AuditFilter = $AuditFilter.split('=')[1].trim()
+				} catch {
+					$AuditFilter = 'Never Configured'
+				}
+			}
+		}
+		if ($CertutilFlag) {
+			[string]$SANFlag = $CertutilFlag | Select-String ' EDITF_ATTRIBUTESUBJECTALTNAME2 -- 40000 \('
+			if ($SANFlag) {
+				$SANFlag = 'Yes'
+			} else {
+				$SANFlag = 'No'
+			}
+		}
+		Add-Member -InputObject $_ -MemberType NoteProperty -Name AuditFilter -Value $AuditFilter -Force
+		Add-Member -InputObject $_ -MemberType NoteProperty -Name CAEnrollmentEndpoint -Value $CAEnrollmentEndpoint -Force
+		Add-Member -InputObject $_ -MemberType NoteProperty -Name CAFullName -Value $CAFullName -Force
+		Add-Member -InputObject $_ -MemberType NoteProperty -Name CAHostname -Value $CAHostname -Force
+		Add-Member -InputObject $_ -MemberType NoteProperty -Name CAHostDistinguishedName -Value $CAHostDistinguishedName -Force
+		Add-Member -InputObject $_ -MemberType NoteProperty -Name SANFlag -Value $SANFlag -Force
+	}
+	
 	$ADCSObjects += Get-CAHostObject -ADCSObjects $ADCSObjects
 	$CAHosts = Get-CAHostObject -ADCSObjects $ADCSObjects
 	$CAHosts | ForEach-Object { $SafeUsers += '|' + $_.Name }
